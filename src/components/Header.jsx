521a1c987e144e2ec6ab40a9e4766318064ade2a
Tridib11 <tridibghosh12345@gmail.com>
2024-04-09 05:18:46 +0530

    File added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,17 +37,27 @@ function Header() {
     },
   ];
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-between p-5">
       <div className="flex gap-8 items-center ">
         <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
-        {menu.map((item) => (
-          <HeaderItem name={item.name} Icon={item.icon} />
-        ))}
+        <div className="hidden md:flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+        <div className="flex md:hidden gap-8">
+          {menu.map((item,index) =>index<3&&(
+            <HeaderItem Icon={item.icon} />
+          ))}
+          <div className="md:hidden">
+            <HeaderItem Icon={HiDotsVertical}/>
+          </div>
+        </div>
       </div>
-        <img
-          src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-          className="w-[40px] rounded-full"
-        />      
+      <img
+        src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+        className="w-[40px] rounded-full"
+      />
     </div>
   );
 }
