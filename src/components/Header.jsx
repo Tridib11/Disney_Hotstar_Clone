e4f7c9f80d5cd1df003d1b06e92713cf75e38e79
Tridib11 <tridibghosh12345@gmail.com>
2024-04-09 03:46:21 +0530

    file added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,35 @@
 import React from 'react'
 import logo from './../assets/Images/logo.png'
+import { HiHome,HiMagnifyingGlass ,HiStar,HiPlayCircle,HiTv } from 'react-icons/hi2'
+import { HiPlus,HiDotsVertical } from 'react-icons/hi'
 function Header() {
+  const menu=[
+    {
+      name:"HOME",
+      icon:HiHome
+    },
+    {
+      name:"SEARCH",
+      icon:HiMagnifyingGlass
+    },
+    {
+      name:'WATCH LIST',
+      icon:HiPlus
+    },
+    {
+      name:"ORGINALS",
+      icon:HiStar
+    },
+    {
+      name:"MOVIES",
+      icon:HiPlayCircle
+    },
+    {
+      name:"SERIES",
+      icon:HiTv
+    }
+
+  ]
   return (
     <div>
       <img src={logo} className='w-[80px] md:w-[115p] object-cover' />
