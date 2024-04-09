24ccbf099a5b49c8b897e899631e0b8644c4e6ed
Tridib11 <tridibghosh12345@gmail.com>
2024-04-10 01:36:34 +0530

    File added

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GlobalApi from "../Services/GlobalApi";
 
 function MovieList({ genreId }) {
+  const [movieList, setMovieList] = useState([]);
   useEffect(() => {
     getMovieByGenreId();
   }, []);
   const getMovieByGenreId = () => {
     GlobalApi.getMovieByGenreId(genreId).then((resp) => {
       console.log(resp.data.results);
+      setMovieList(resp.data.results);
     });
   };
   return <div>MovieList</div>;
