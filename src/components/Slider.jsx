4abd5858d88a2a8685a9566c60e8e897b51d68eb
Tridib11 <tridibghosh12345@gmail.com>
2024-04-09 06:15:30 +0530

    File added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react'
+import GlobalApi from '../Services/GlobalApi'
 
 function Slider() {
   useEffect(()=>{
-    
-  })
+    getTrendingMovies()
+  },[])
+
+  const getTrendingMovies=()=>{
+    GlobalApi.getTrendingVideos.then(resp=>{
+      console.log(resp)
+    })
+  }
   return (
     <div>
       
