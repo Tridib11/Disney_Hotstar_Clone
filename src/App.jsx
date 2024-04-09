704aeeab4655422a573028f93799c9693ed97364
Tridib11 <tridibghosh12345@gmail.com>
2024-04-10 04:12:00 +0530

    File added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import GenreMovieList from "./components/GenreMovieList";
 import Header from "./components/Header";
 import ProductionHouse from "./components/ProductionHouse";
 import Slider from "./components/Slider";
+import Name from "./components/Name";
 import './App.css'; 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Slider/>
       <ProductionHouse/>
       <GenreMovieList/>
+      <Name/>
     </div>
   );
 }
---
+++ src/components/Name.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Name() {
+  return (
+    <div className='text-white flex justify-center items-center pb-6 '>
+      <h3>Made by Tridib Ghosh</h3>
+    </div>
+  )
+}
+
+export default Name
