75443d32f57e38a92a6310eea16c627d07cae225
Tridib11 <tridibghosh12345@gmail.com>
2024-04-10 01:29:47 +0530

    File added

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -7,7 +7,7 @@ function GenreMovieList() {
       {GenresList.genere.map((item,index)=>index<=4&&(
         <div className='p-8 px-8 md:px-16'>
           <h2 className='test-[20px] text-white font-bold'>{item.name}</h2>
-          <MovieList genreId={item.}/>
+          <MovieList genreId={item.id}/>
         </div>
       ))}
     </div>
