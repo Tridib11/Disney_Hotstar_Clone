beca249420422a9809c32379585ea6c8b43e2fad
Tridib11 <tridibghosh12345@gmail.com>
2024-04-09 07:45:08 +0530

    File added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect } from 'react'
-import GlobalApi from '../Services/GlobalApi'
-
+import React, { useEffect, useState } from "react";
+import GlobalApi from "../Services/GlobalApi";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 function Slider() {
-  useEffect(()=>{
-    getTrendingMovies()
-  },[])
+  const [movieList, setMovieList] = useState([]);
+  useEffect(() => {
+    getTrendingMovies();
+  }, []);
 
-  const getTrendingMovies=()=>{
-    GlobalApi.getTrendingVideos.then(resp=>{
-      console.log(resp)
-    })
-  }
+  const getTrendingMovies = () => {
+    GlobalApi.getTrendingVideos.then((resp) => {
+      console.log(resp.data.results);
+      setMovieList(resp.data.results);
+    });
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div>
+        <HiChevronLeft className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer" />
+        <HiChevronRight className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0" />
+      </div>
+      <div className="flex overflow-x-auto w-full px-16 py-4 scrollbar-hide">
+        {movieList.map((item, index) => (
+          <img
+            src={IMAGE_BASE_URL + item.backdrop_path}
+            className="min-w-full md:h-[310px] rounded-md object-cover mr-5  object-top"
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Slider
+export default Slider;
