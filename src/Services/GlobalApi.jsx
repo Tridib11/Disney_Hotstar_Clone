92cff7a67d2c3d9e1fa3102f808df687c0fa5ba7
Tridib11 <tridibghosh12345@gmail.com>
2024-04-10 01:15:40 +0530

    File added

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,11 +1,20 @@
-import axios from "axios";
-const movieBaseUrl="https://api.themoviedb.org/3"
-const api_key='65ea4d1f26cf522dad7bf27e500d09ed'
 //https://https://api.themoviedb.org/3/trending/movie/day?language=en-US?api_key=65ea4d1f26cf522dad7bf27e500d09ed
 
-const getTrendingVideos=axios.get(movieBaseUrl+
-  "/trending/all/day?api_key="+api_key);
+import axios from "axios";
+const movieBaseUrl = "https://api.themoviedb.org/3";
+const api_key = "65ea4d1f26cf522dad7bf27e500d09ed";
+
+const movieByGenreBaseURL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=65ea4d1f26cf522dad7bf27e500d09ed";
+
+const getTrendingVideos = axios.get(
+  movieBaseUrl + "/trending/all/day?api_key=" + api_key
+);
+
+const getMovieByGenreId = (id) =>
+  axios.get(movieByGenreBaseURL + "&with_genres=" + id);
 
 export default {
-  getTrendingVideos
-}
+  getTrendingVideos,
+  getMovieByGenreId,
+};
