65d0fadbccf29e511cf4adc06050d85eaa8716ba
Tridib11 <tridibghosh12345@gmail.com>
2024-04-10 01:19:16 +0530

    File added

---
+++ src/components/GenreMovieList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GenresList from '../Constant/GenresList'
+function GenreMovieList() {
+  return (
+    <div>
+      {GenresList.genere.map((item)=>(
+        
+      ))}
+    </div>
+  )
+}
+
+export default GenreMovieList
