ec55dc9045dd43dba7a88db13d22b622f5298138
Tridib11 <tridibghosh12345@gmail.com>
2024-04-10 03:49:15 +0530

    File added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import GenreMovieList from "./components/GenreMovieList";
 import Header from "./components/Header";
 import ProductionHouse from "./components/ProductionHouse";
 import Slider from "./components/Slider";
-
+import './App.css'; 
 function App() {
   return (
     <div className="">
