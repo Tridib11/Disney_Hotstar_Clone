21ceed8da65753008b3d44e398a4e4b1af01c273
Tridib11 <tridibghosh12345@gmail.com>
2024-04-09 08:03:24 +0530

    File added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 import GlobalApi from "../Services/GlobalApi";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+
+const screenWidth=window.innerWidth
 function Slider() {
   const [movieList, setMovieList] = useState([]);
   const elementRef = useRef();
@@ -17,20 +19,20 @@ function Slider() {
   };
 
   const sliderRight = (element) => {
-    element.scrollLeft += 800;
+    element.scrollLeft += screenWidth-110;
   };
   const sliderLeft = (element) => {
-    element.scrollLeft -= 800;
+    element.scrollLeft -= screenWidth-110;
   };
   return (
     <>
       <div>
         <HiChevronLeft
-          className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"
+          className="hidden md:block text-white text-[30px] absolute mx-8 mt-[250px] cursor-pointer"
           onClick={() => sliderLeft(elementRef.current)}
         />
         <HiChevronRight
-          className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
+          className="hidden md:block text-white text-[30px] absolute mx-8 mt-[250px] cursor-pointer right-0"
           onClick={() => sliderRight(elementRef.current)}
         />
       </div>
@@ -41,7 +43,7 @@ function Slider() {
         {movieList.map((item, index) => (
           <img
             src={IMAGE_BASE_URL + item.backdrop_path}
-            className="min-w-full md:h-[310px] rounded-md object-cover mr-5  object-top"
+            className="min-w-full md:h-[500px] rounded-md object-cover mr-5  object-top"
           />
         ))}
       </div>
