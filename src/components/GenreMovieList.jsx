814a62a7ca2770f1b43e0dd3071b35b9988323b1
Tridib11 <tridibghosh12345@gmail.com>
2024-04-10 01:29:36 +0530

    File added

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import GenresList from '../Constant/GenresList'
+import MovieList from './MovieList'
 function GenreMovieList() {
   return (
     <div>
-      {GenresList.genere.map((item)=>(
-        
+      {GenresList.genere.map((item,index)=>index<=4&&(
+        <div className='p-8 px-8 md:px-16'>
+          <h2 className='test-[20px] text-white font-bold'>{item.name}</h2>
+          <MovieList genreId={item.}/>
+        </div>
       ))}
     </div>
   )
