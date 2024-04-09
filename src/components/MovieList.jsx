e21ec30899c86a9c31b7976fc0894333e9db7b8f
Tridib11 <tridibghosh12345@gmail.com>
2024-04-10 01:36:55 +0530

    File added

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -12,7 +12,11 @@ function MovieList({ genreId }) {
       setMovieList(resp.data.results);
     });
   };
-  return <div>MovieList</div>;
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 export default MovieList;
